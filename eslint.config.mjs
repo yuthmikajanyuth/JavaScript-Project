c78465cd35d3f407a3e3d9edcb79daa46d2785d0
yuthmikajanyuth <159243666+yuthmikajanyuth@users.noreply.github.com>
2025-04-26 11:26:30 +0530

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,16 @@
 import { defineConfig } from "eslint/config";
 import globals from "globals";
 
-export default defineConfig([
-  {
-    files: ["src/*.{js,mjs,cjs}"],
-    languageOptions: { globals: globals.browser },
-    rules: {
-      "no-unused-vars": "error", // or "error" to treat it as an error
+export default defineConfig({
+  overrides: [
+    {
+      files: ["src/*.{js,mjs,cjs}"],
+      languageOptions: {
+        globals: globals.browser,
+      },
+      rules: {
+        "no-unused-vars": "error", // Treat unused variables as errors
+      },
     },
-  },
-]);
+  ],
+});
